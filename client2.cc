9e71a5b45ba7381c06d228a1934dd5c565e85046
luong <gialuong9820@gmail.com>
2022-05-06 11:22:20 +0000

    client2

--- client2.cc
+++ client2.cc
@@ -5,20 +5,21 @@
 #include <netinet/in.h>
 
 
-void StartMDNS(mdns::MDnsPub &pub) {
+void StartMDNS(mdns::MDnsPub &pub, mdns::MDnsSub &sub) {
     pub.Register();
+    sub.ScanRecord([](){}, [](){});
 }
 
 int main() {
     mdns::MDnsSub sub("client1", "_http._tcp");
     mdns::MDnsPub pub("client2", "_http._tcp");
-    StartMDNS(pub);
+    StartMDNS(pub, sub);
 
     std::string ip_address = sub.GetIpAddress();
+    std::string port = sub.get_record_by_key("port");
 
     printf("ip_address: %s\n", ip_address.data());
-
-
+    printf("port: %s\n", port.data());
 
     return 0;
 }
